d01129cc88c8c9a2372fbf3a6d17a553905429d6
vgroux <victorgroux99@gmail.com>
2024-03-24 02:04:22 +0100

    oke

--- Scripts/PlayerManager.cs
+++ Scripts/PlayerManager.cs
@@ -77,6 +77,7 @@ public class Player : MonoBehaviour
 	}
 	private void OnMovementCanceled(InputAction.CallbackContext context)
 	{
+		transform.forward = rb.velocity.normalized;
 		rb.velocity = Vector3.zero;
 	}
 
